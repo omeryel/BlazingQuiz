e15b898551e234384b2057c56f8a814af5ef9dd3
Omer YEL <omeryel54@gmail.com>
2024-12-15 16:52:02 +0300

    [ Create Custom Modal Popup Component. View Quiz Questions in Modal Popup in Admin Panel ]

---
+++ BlazingQuiz.Shared/ModalSize.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingQuiz.Shared
+{
+    public enum ModalSize
+    {
+        Small,
+        Default,
+        Large,
+        ExtraLarge
+    }
+
+    
+}
